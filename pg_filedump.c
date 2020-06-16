ee367115cbdaf8e54c1f6990bb7c5311a244bbf8
Mr. PostgreSQL13X <postgresql13x@gmail.com>
2020-06-16 11:54:49 -0400

    The 2 version

--- pg_filedump.c
+++ pg_filedump.c
@@ -11,8 +11,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Possible return codes from option validation routine.
+ *  * pg_filedump doesn't do much with them now but maybe in
+ *   * the future... */
+typedef enum optionReturnCodes
+{
+    OPT_RC_VALID,               /* All options are valid */
+    OPT_RC_INVALID,             /* Improper option string */
+    OPT_RC_FILE,                /* File problems */
+    OPT_RC_DUPLICATE,           /* Duplicate option encountered */
+    OPT_RC_COPYRIGHT            /* Copyright should be displayed */
+}   optionReturnCodes;
+
+
 int main(int argc, char* argv[])
 {
+    /* If there is a parameter list, validate the options */
+    unsigned int validOptions;
+
+    validOptions = (argc < 2) ? OPT_RC_COPYRIGHT : OPT_RC_VALID;
+
 	printf("Hello, PostgreSQL!\n");
 
 	return 0;
